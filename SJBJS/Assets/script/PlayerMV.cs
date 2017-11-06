6a7e4bde78cb8e0116362cb9393f9ea3781366bb
정상동 <dlwjdtnrdjaa@naver.com>
2017-11-06 17:43:23 +0900

    유니티 맵환경 작업스타트
    플레이어, 카메라 무브 작성

---
+++ SJBJS/Assets/script/PlayerMV.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMV : MonoBehaviour
+{
+    public float speed; //이동 속도
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = -1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //왼쪽방향으로 speed만큼 이동하게됩니다.
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        //x좌표가 -12.8보다 작아지게 되면...
+        if (transform.position.x < -12.8f)
+        {
+            //위치를 0, 0 으로 옮겨줍니다.
+            transform.position = new Vector2(0, 0);
+        }
+    }
+}
---
+++ SJBJS/Assets/script/SpwanGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpwanGround : MonoBehaviour {
+
+    public bool enableSpawn = true;
+    public GameObject ground;
+   
+    void SpawnGround()
+    {
+         if (enableSpawn)
+        {
+                  
+            GameObject ground1 = Instantiate(ground, new Vector3(Random.Range(-10f, 10f), -3.0f, -1.0f), Quaternion.identity);
+            GameObject ground2 = Instantiate(ground, new Vector3(Random.Range(-10f, 10f), -3.0f, -1.0f), Quaternion.identity);
+            GameObject ground3 = Instantiate(ground, new Vector3(Random.Range(-10f, 10f), -3.0f, -1.0f), Quaternion.identity);
+
+        }
+    }
+    void Start()
+    {
+        SpawnGround();
+    }
+    void Update()
+    {
+
+    }
+}
